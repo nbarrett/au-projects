5422e32bfbc82a1d266ede84b64923bc44c3ea01
nbarrett <nick.barrett36@me.com>
2021-05-29 23:43:10 +0100

    - Company edit now fully working

--- client/src/pages/companies/Companies.tsx
+++ client/src/pages/companies/Companies.tsx
@@ -1,20 +1,26 @@
 import { Helmet } from "react-helmet";
-import { Box, Button, Container } from "@material-ui/core";
+import { Box, Container } from "@material-ui/core";
 import { useSetRecoilState } from "recoil";
 import * as React from "react";
 import { useEffect } from "react";
 import CompaniesList from "./CompaniesList";
 import { ToolbarButton } from "../../models/toolbar-models";
 import { toolbarButtonState } from "../../atoms/navbar-atoms";
+import { log } from '../../util/logging-config';
 import useCompanyData from '../../hooks/use-company-data';
 
 export default function Companies() {
     const setToolbarButtons = useSetRecoilState<ToolbarButton[]>(toolbarButtonState);
+    const companyData = useCompanyData();
 
     useEffect(() => {
         setToolbarButtons([
             "export",
             "import"])
+    })
+
+    useEffect(() => {
+        log.info(companyData.companies);
     })
 
     return (
