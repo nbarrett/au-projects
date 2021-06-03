1ceb01f36fd60caf74feb86ea64040220cf9698f
nbarrett <nick.barrett36@me.com>
2021-06-04 00:55:13 +0100

    - fixed drawer props for mobile breakpoints

--- client/src/display/display.ts
+++ client/src/display/display.ts
@@ -1,8 +1,10 @@
 export const hidden = {display: "none"};
+
 export const lgUpHidden = {display: {xs: "block", lg: "none", xl: "none"}};
+export const lgDownHidden = {display: {xs: "none", lg: "block", xl: "block"}};
+
 export const lgUpVisible = {display: {xs: "block", lg: "block", xl: "none"}};
 export const lgDownVisible = {display: {xs: "block", lg: "block", xl: "none"}};
-export const lgDownHidden = {display: {xs: "none", lg: "none", xl: "block"}};
 export const xsHidden = {display: {xs: "none", sm: "block"}};
 export const smHidden = {display: {xs: "block", sm: "none", md: "block"}};
 export const mdHidden = {display: {xs: "block", md: "none", lg: "block"}};
