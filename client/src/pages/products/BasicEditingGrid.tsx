c89e480e9194022d70409b6096f2b64ef0662655
nbarrett <nick.barrett36@me.com>
2021-06-22 00:55:32 +0100

    - product edting more reliable now: - onEditCellChangeCommitted dumped in favour of onEditRowModelChange (happens per keypress which could be better for row calcs)

--- client/src/pages/products/BasicEditingGrid.tsx
+++ client/src/pages/products/BasicEditingGrid.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { useCallback, useEffect } from 'react';
 import { DataGrid, GridEditCellPropsParams, GridEditRowModelParams, GridToolbar } from '@material-ui/data-grid';
 import useProductData from '../../hooks/use-product-data';
 import { productColumns } from './ProductsTable';
@@ -14,48 +13,38 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import SaveIcon from '@material-ui/icons/Save';
 import UndoIcon from '@material-ui/icons/Undo';
 import { useSnackbarNotification } from '../../snackbarNotification';
+import map from 'lodash/map';
 
 export default function BasicEditingGrid(props: { products: WithUid<Product>[] }) {
 
     const productData = useProductData()
-    const [editRowsModel, setEditRowsModel] = React.useState({});
     const inputRows = props.products.map(item => toRow(item));
     const notification = useSnackbarNotification();
-    const handleEditRowModelChange = useCallback(
-        (params: GridEditRowModelParams) => {
-            setEditRowsModel(params.model);
-            log.debug("params.model", params.model);
-        },
-        [],
-    );
 
-    const handleEditCellChangeCommitted = React.useCallback(
-        ({id, field, props}: GridEditCellPropsParams) => {
-            log.info("handleEditCellChangeCommitted:", id, field, props)
-            const updatedProduct = findProduct(id);
-            if (updatedProduct) {
-                updatedProduct.data[field] = cellValue(field, props);
-                log.info("updatedProduct:", updatedProduct);
-                productData.setSingleProduct(updatedProduct)
-            } else {
-                notification.error(`Cant update product based with id: ${id}, ${field}, ${JSON.stringify(props)}`)
-            }
-        }, []
-    );
+    function handleEditRowModelChange(params: GridEditRowModelParams) {
+        map(params.model, ((editedData, uid) => {
+            map(editedData, ((value, field) => {
+                log.debug("field", field, "value", value.value, "uid", uid);
+                const updatedProduct = findProduct(uid);
+                if (updatedProduct) {
+                    updatedProduct.data[field] = cellFieldValue(field, value.value);
+                    log.debug("updatedProduct:", updatedProduct);
+                    productData.setSingleProduct(updatedProduct)
+                } else {
+                    notification.error(`Cant update product based with id: ${uid}, ${field}, ${JSON.stringify(value)}`)
+                }
+            }))
+        }))
+    }
 
-    function cellValue(field: string, props) {
-        const value = get(props, "value");
-        log.info("value:", value)
+    function cellFieldValue(field: string, value) {
+        log.debug("value:", value)
         return columnHasNumber(productColumns.find(item => item.fieldName.endsWith(field))) ? +value : value
     }
 
     function findProduct(id) {
         return cloneDeep(productData.products.find(item => item.uid === id));
     }
-
-    useEffect(() => {
-        log.info("viewing", props.products.length, "filtered products", productData.products.length, "product data records")
-    })
 
     function toGridColumn(item: DataColumn) {
         return {
@@ -112,10 +101,8 @@ export default function BasicEditingGrid(props: { products: WithUid<Product>[] }
             <div style={{height: window.innerHeight - 200, width: '100%'}}>
                 <DataGrid components={{
                     Toolbar: GridToolbar,
-                }} editRowsModel={editRowsModel}
-                          isCellEditable={(params) => true}
+                }}isCellEditable={(params) => true}
                           disableSelectionOnClick
-                          onEditCellChangeCommitted={handleEditCellChangeCommitted}
                           onEditRowModelChange={handleEditRowModelChange}
                           checkboxSelection rows={inputRows}
                           columns={productColumns.map(item => toGridColumn(item))}/>
